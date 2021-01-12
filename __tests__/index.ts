597583c38d5479e5a50f28a658a7f8968bd80011
Alistair Smith <alistairsmith01@gmail.com>
2021-01-12 20:29:22 +0000

    chore: Add tests for type of properties

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -3,16 +3,15 @@ import {
   NullableDate,
   NullableNumber,
   NullableString,
-  getTypeForProperty,
+  getInstanceOfProperty,
   NullableBoolean,
   Property,
   Types,
-  WithNullables,
   RemapProperties,
   getAllPropertyKeys,
+  getTypeOfProperty,
 } from "../src";
 
-@WithNullables
 class Person extends Entity {
   @Property
   name!: NullableString;
@@ -31,23 +30,29 @@ const person = new Person() as RemapProperties<Person>;
 
 describe("Check types", () => {
   test("Correctly generate a class with a nullable string", () => {
-    const type = new (getTypeForProperty(person, "name"))();
+    const type = new (getInstanceOfProperty(person, "name"))();
     expect(type).toBeInstanceOf(Types.NullableString);
   });
 
   test("Correctly generate a class with a nullable date", () => {
-    const type = new (getTypeForProperty(person, "birthday"))();
+    const type = new (getInstanceOfProperty(person, "birthday"))();
     expect(type).toBeInstanceOf(Types.NullableDate);
   });
 
   test("Correctly generate a class with a nullable number", () => {
-    const type = new (getTypeForProperty(person, "age"))();
+    const type = new (getInstanceOfProperty(person, "age"))();
     expect(type).toBeInstanceOf(Types.NullableNumber);
   });
 
   test("Correctly generate a class with a nullable bool", () => {
-    const type = new (getTypeForProperty(person, "is_admin"))();
+    const type = new (getInstanceOfProperty(person, "is_admin"))();
     expect(type).toBeInstanceOf(Types.NullableBoolean);
+  });
+
+  test("Correctly get the types of all properties", () => {
+    const keys = getAllPropertyKeys(person);
+    const [name, age, birthday, is_admin] = keys.map((key) => getTypeOfProperty(person, key));
+    expect([name, age, birthday, is_admin]).toEqual(["string", "number", "date", "boolean"]);
   });
 });
 
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,48 @@
 import "reflect-metadata";
 
-import { Entity, NullableBoolean, NullableDate, NullableNumber, NullableString, Types } from "./structs";
+import {
+  availableTypes,
+  Entity,
+  NullableBoolean,
+  NullableDate,
+  NullableNumber,
+  NullableString,
+  Types,
+} from "./structs";
 
-export const reflectionKeys = {
-  classKeys: "ts:nullables:keys",
-};
+export const reflectionKeys = { classKeys: "ts:nullables:keys" };
 
 export const Property: PropertyDecorator = (target, property) => {
   const existing = Reflect.getMetadata(reflectionKeys.classKeys, target) || [];
   Reflect.defineMetadata(reflectionKeys.classKeys, [...existing, property], target);
 };
 
-export const WithNullables: ClassDecorator = () => {
-  return void 0;
-};
-
-export function getAllPropertyKeys(target: Object): string[] {
+export function getAllPropertyKeys<T extends Object>(target: T): (keyof T)[] {
   return Reflect.getMetadata(reflectionKeys.classKeys, target) || [];
 }
 
-export function getTypeForProperty<T extends Entity, K extends keyof T>(
+export function getInstanceOfProperty<T extends Entity, K extends keyof T>(
   target: T,
   property: K
 ): typeof Types[keyof typeof Types] {
   return Reflect.getMetadata("design:type", target, property as string);
+}
+
+export function getTypeOfProperty<T extends Entity, K extends keyof T>(
+  target: T,
+  property: K
+): typeof availableTypes[number] {
+  const instance = getInstanceOfProperty(target, property);
+  switch (instance.name as keyof typeof Types) {
+    case "NullableBoolean":
+      return "boolean";
+    case "NullableDate":
+      return "date";
+    case "NullableNumber":
+      return "number";
+    case "NullableString":
+      return "string";
+  }
 }
 
 export type ParsePotentiallyNullableType<T> = T extends NullableBoolean
